447720b940de083561d4c5b3561d81d7bd121d66
ADNAN KILIÇ <adnankilic@MacBookPro.fritz.box>
2025-06-23 10:52:53 +0200

    added sql for tech and data science mock interviews

--- SQLZoo.net Tutorials/5. SUM and COUNT.sql
+++ SQLZoo.net Tutorials/5. SUM and COUNT.sql
@@ -1,49 +1,72 @@
-# 1. Show the total population of the world.
+/*
+1. Show the total population of the world.
+*/
 
-SELECT SUM(population) FROM world
+SELECT 
+    SUM(population) AS total_population
+FROM world
 ---------------------------------------------------------------------------------------------------------------------------------------
+/*
+2. List all the continents - just once each.
+*/
 
-# 2. List all the continents - just once each.
 
 SELECT DISTINCT continent FROM world
 ---------------------------------------------------------------------------------------------------------------------------------------
+/*
+3. Give the total GDP of Africa
+*/
 
-# 3. Give the total GDP of Africa
-
-SELECT SUM(gdp) FROM world
+SELECT 
+    SUM(gdp) AS total_gdp
+FROM world
 WHERE continent = 'Africa'
 ---------------------------------------------------------------------------------------------------------------------------------------
+/*
+4. How many countries have an area of at least 1000000
+*/
 
-# 4. How many countries have an area of at least 1000000
-
-SELECT COUNT(name) FROM world
+SELECT 
+    COUNT(name) AS name_counts
+FROM world
 WHERE area > 1000000
 ---------------------------------------------------------------------------------------------------------------------------------------
+/*
+5. What is the total population of ('Estonia', 'Latvia', 'Lithuania')
+*/
 
-# 5. What is the total population of ('Estonia', 'Latvia', 'Lithuania')
-
-SELECT SUM(population) FROM world
+SELECT 
+    SUM(population) AS total_population
+FROM world
 WHERE name IN ('Estonia', 'Latvia', 'Lithuania')
 ---------------------------------------------------------------------------------------------------------------------------------------
+/*
+6. For each continent show the continent and number of countries.
+*/
 
-# 6. For each continent show the continent and number of countries.
-
-SELECT continent, COUNT(name)
+SELECT 
+    continent
+    , COUNT(name) AS name_counts
 FROM world
 GROUP BY continent
 ---------------------------------------------------------------------------------------------------------------------------------------
+/*
+7. For each continent show the continent and number of countries with populations of at least 10 million.
+*/
 
-# 7. For each continent show the continent and number of countries with populations of at least 10 million.
-
-SELECT continent, COUNT(name)
+SELECT 
+    continent
+    , COUNT(name) as name_counts
 FROM world
 WHERE population >= 10000000
 GROUP BY continent
 ---------------------------------------------------------------------------------------------------------------------------------------
+/*
+8. List the continents that have a total population of at least 100 million.
+*/
 
-# 8. List the continents that have a total population of at least 100 million.
-
-SELECT continent
+SELECT 
+    continent
 FROM world
 GROUP BY continent
 HAVING SUM(population) >= 100000000
--- SQLZoo.net Tutorials/7. More JOIN Operations.sql
+++ SQLZoo.net Tutorials/7. More JOIN Operations.sql
@@ -1,42 +1,55 @@
-# 1. List the films where the yr is 1962 [Show id, title]
+/*
+1. List the films where the yr is 1962 [Show id, title]
+*/
 
 SELECT 
-  id, 
-  title
+  id
+  ,title
 FROM movie
 WHERE yr=1962
-
-# 2. Give year of 'Citizen Kane'.
+-----------------------------------------------------------------------------------------------------------------------------------------
+/*
+2. Give year of 'Citizen Kane'.
+*/
 
 SELECT
   yr
 FROM movie
 WHERE title = 'Citizen Kane'
-
-# 3. List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+-----------------------------------------------------------------------------------------------------------------------------------------
+/*
+3. List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+*/
 
 SELECT
-  id,
-  title,
-  yr
+  id
+  ,title
+  ,yr
 FROM movie
 WHERE LOWER(title) LIKE '%star trek%'
-
-# 4. What id number does the actor 'Glenn Close' have?
+ORDER BY yr
+-----------------------------------------------------------------------------------------------------------------------------------------
+/*
+4. What id number does the actor 'Glenn Close' have?
+*/
 
 SELECT
   id
 FROM actor
 WHERE name = 'Glenn Close'
-
-# 5. What is the id of the film 'Casablanca'
+-----------------------------------------------------------------------------------------------------------------------------------------
+/*
+5. What is the id of the film 'Casablanca'
+*/
 
 SELECT
   id
 FROM movie
 WHERE title = 'Casablanca'
-
-# 6. Obtain the cast list for 'Casablanca'.
+-----------------------------------------------------------------------------------------------------------------------------------------
+/*
+6. Obtain the cast list for 'Casablanca'.
+*/
 
 SELECT 
   name 
@@ -44,8 +57,10 @@ FROM actor
 INNER JOIN casting ON actor.id = casting.actorid
 INNER JOIN movie ON casting.movieid = movie.id
 WHERE title = 'Casablanca'
-
-# 7. Obtain the cast list for the film 'Alien'
+-----------------------------------------------------------------------------------------------------------------------------------------
+/*
+7. Obtain the cast list for the film 'Alien'
+*/
 
 SELECT 
   name 
@@ -53,8 +68,10 @@ FROM actor
 INNER JOIN casting ON actor.id = casting.actorid
 INNER JOIN movie ON casting.movieid = movie.id
 WHERE title = 'Alien'
-
-# 8. List the films in which 'Harrison Ford' has appeared
+-----------------------------------------------------------------------------------------------------------------------------------------
+/*
+8. List the films in which 'Harrison Ford' has appeared
+*/
 
 SELECT 
   title
@@ -62,9 +79,11 @@ FROM actor
 INNER JOIN casting ON actor.id = casting.actorid
 INNER JOIN movie ON casting.movieid = movie.id
 WHERE name = 'Harrison Ford' 
-
-# 9. List the films where 'Harrison Ford' has appeared - but not in the starring role. 
-# [Note: the ord field of casting gives the position of the actor. If ord=1 then this actor is in the starring role]
+-----------------------------------------------------------------------------------------------------------------------------------------
+/*
+9. List the films where 'Harrison Ford' has appeared - but not in the starring role. 
+[Note: the ord field of casting gives the position of the actor. If ord=1 then this actor is in the starring role]
+*/
 
 SELECT
  title
@@ -72,48 +91,51 @@ FROM actor
 INNER JOIN casting ON casting.actorid = actor.id
 INNER JOIN movie ON movie.id = casting.movieid
 WHERE name = 'Harrison Ford' AND ord <> 1
-
-# 10. List the films together with the leading star for all 1962 films.
+-----------------------------------------------------------------------------------------------------------------------------------------
+/*
+10. List the films together with the leading star for all 1962 films.
+*/
 
 SELECT
- title,
- name
+ title
+ ,name
 FROM actor
 INNER JOIN casting ON casting.actorid = actor.id
 INNER JOIN movie ON movie.id = casting.movieid
 WHERE yr = 1962 AND ord = 1
-
-
-# 11. Which were the busiest years for 'Rock Hudson', show the year and the number of movies he made each year for any year in which he made more than 2 movies.
+-----------------------------------------------------------------------------------------------------------------------------------------
+/*
+11. Which were the busiest years for 'Rock Hudson', show the year and the number of movies he made each year for any year in which he made more than 2 movies.
+*/
 
 SELECT 
-  movie.yr, 
-  COUNT(*)
+  movie.yr
+  ,COUNT(*)
 FROM movie
 INNER JOIN casting ON casting.movieid = movie.id
 INNER JOIN actor ON actor.id = casting.actorid
 WHERE name = 'Rock Hudson'
 GROUP BY yr
-HAVING COUNT(title) >= 2
+HAVING COUNT(title) > 2
+-----------------------------------------------------------------------------------------------------------------------------------------
+/*
+12. List the film title and the leading actor for all of the films 'Julie Andrews' played in.
 
+Did you get "Little Miss Marker twice"?
+Julie Andrews starred in the 1980 remake of Little Miss Marker and not the original(1934).
 
-
-# 12. List the film title and the leading actor for all of the films 'Julie Andrews' played in.
-
-# Did you get "Little Miss Marker twice"?
-# Julie Andrews starred in the 1980 remake of Little Miss Marker and not the original(1934).
-
-# Title is not a unique field, create a table of IDs in your subquery
+Title is not a unique field, create a table of IDs in your subquery
+*/
 
 SELECT
- DISTINCT m.title,
- a.name
+ DISTINCT m.title
+ ,a.name
 FROM (
 
 	SELECT
-	 movie.id,
-	 title,
-	 name
+	 movie.id
+	 ,title
+	 ,name
 	FROM movie
 	INNER JOIN casting ON casting.movieid = movie.id
 	INNER JOIN actor ON casting.actorid = actor.id
@@ -121,23 +143,25 @@ FROM (
 
 ) AS m
 INNER JOIN (SELECT 
-              actor.*, 
-              casting.movieid AS movieid
+              actor.*
+              ,casting.movieid AS movieid
             FROM actor
             INNER JOIN casting ON casting.actorid = actor.id
             WHERE casting.ord = 1
             ) AS a
 ON m.id = a.movieid
-
-# 13. Obtain a list, in alphabetical order, of actors who've had at least 15 starring roles.
+-----------------------------------------------------------------------------------------------------------------------------------------
+/*
+13. Obtain a list, in alphabetical order, of actors who've had at least 15 starring roles.
+*/
 
 SELECT
   name
 FROM(
 
 	SELECT 
-	 name,
-	 COUNT(movieid) AS counts
+	 name
+	 ,COUNT(movieid) AS counts
 	FROM movie
 	INNER JOIN casting ON casting.movieid = movie.id
 	INNER JOIN actor ON casting.actorid = actor.id
@@ -147,22 +171,24 @@ FROM(
 
 ) AS m
 ORDER BY name 
-
-# 14. List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
+-----------------------------------------------------------------------------------------------------------------------------------------
+/*
+14. List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
+*/
 
 SELECT
- title,
- COUNT(actorid) AS counts
+ title
+ ,COUNT(actorid) AS counts
 FROM movie
 INNER JOIN casting ON casting.movieid = movie.id
 INNER JOIN actor ON casting.actorid = actor.id
 WHERE yr = 1978
 GROUP BY title
 ORDER BY counts DESC, title
-
-# 15. List all the people who have worked with 'Art Garfunkel'.
-
-
+-----------------------------------------------------------------------------------------------------------------------------------------
+/*
+15. List all the people who have worked with 'Art Garfunkel'.
+*/
 
 SELECT 
 	a.name
